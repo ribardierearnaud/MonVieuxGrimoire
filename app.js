716f57ff929b114c2b4a9c090235596fd02aa463
Arnaud Ribardière <arnaud.ribardiere@gmail.com>
2024-01-08 12:46:11 +0100

    Addition of cors and helmet

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const mongoose = require('mongoose');
 const bookRoutes = require('./routes/book');
 const userRoutes = require('./routes/user');
 const path = require('path');
+const helmet = require('helmet');
+const cors = require('cors');
+
+
 
 mongoose.connect('mongodb+srv://arnaudribardiere:XAZ2iNu18b2yeUTw@cluster0.uqyafzw.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -13,14 +17,31 @@ mongoose.connect('mongodb+srv://arnaudribardiere:XAZ2iNu18b2yeUTw@cluster0.uqyaf
 
 const app = express();
 
+// On appelle Helmet en customisant le Content-Security-Policy header pour autoriser l'affichage des images
+
+app.use(
+  helmet({
+    crossOriginResourcePolicy: {policy: "cross-origin"}
+    },
+  ),
+);
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:4000"],
+  }),
+);
+
 app.use(express.json());
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
+
+
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+//     next();
+// });
 
 app.use('/api/books', bookRoutes);
 app.use('/api/auth', userRoutes);
